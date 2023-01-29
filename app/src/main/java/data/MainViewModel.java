a2ca64c945851715707ebb15f9865f56f35c6b19
Padma2code <padmavalli.aluri@gmail.com>
2023-01-29 00:50:22 -0500

    Finished Lab2 Work

--- app/src/main/java/data/MainViewModel.java
+++ app/src/main/java/data/MainViewModel.java
@@ -1,6 +1,10 @@
 package data;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 public  class MainViewModel extends ViewModel{
-    public String editTextContents;
+    public String editTextContents = "You Clicked Me!";
+   // public MutableLiveData<String> editTextContents = new MutableLiveData<>();
+
+    public MutableLiveData<Boolean> isSelected = new MutableLiveData<>(false);
 }
--- app/src/main/java/ui/MainActivity.java
+++ app/src/main/java/ui/MainActivity.java
@@ -6,16 +6,13 @@ import androidx.lifecycle.ViewModelProvider;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import algonquin.cst2335.alur0001.databinding.ActivityMainBinding;
 import data.MainViewModel;
-
-/*Room T318, THursday 10am (Tomorrow)
-*/
-
-
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding variableBinding;
@@ -31,20 +28,65 @@ public class MainActivity extends AppCompatActivity {
         //EditText et = findViewById(R.id.editText);
 
         variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        variableBinding.mySwitch.setChecked(viewModel.isSelected.getValue());
 
+        // Get values from View Model by observing
+        viewModel.isSelected.observe(this, newBooleanValue -> {
+                variableBinding.mySwitch.setChecked(newBooleanValue);
+                variableBinding.myCheckBox.setChecked(newBooleanValue);
+                variableBinding.myRadio.setChecked(newBooleanValue);
+        });
+
+
+            //Set Toast for Image BTN Height and Width
+            variableBinding.imageBTN.setOnClickListener( imageBTN -> {
+
+
+                Toast
+                        .makeText(MainActivity.this,  "The width = " + String.valueOf(variableBinding.imageBTN.getWidth()) + " and height = " + String.valueOf(variableBinding.imageBTN.getHeight()),Toast.LENGTH_LONG)
+                        .show();
+
+            });
+
+            //Set Toast on Compound buttons status change
+            variableBinding.mySwitch.setOnCheckedChangeListener(( whichButton , isChecked) -> {
+            viewModel.isSelected.postValue(isChecked);
+
+            Toast
+                    .makeText(MainActivity.this,  "The value is now:"+isChecked,Toast.LENGTH_LONG)
+                    .show();
+
+        });
+
+        //Get Text value from View Model for edittext,textview and button
         variableBinding.editText.setText(viewModel.editTextContents);
         variableBinding.TextviewHelloWorld.setText(viewModel.editTextContents);
         variableBinding.button.setText(viewModel.editTextContents);
+
+        //changing the value to true for compound buttons
+        variableBinding.mySwitch.setChecked(true);
+        variableBinding.myRadio.setChecked(true);
+        variableBinding.myCheckBox.setChecked(true);
         setContentView(variableBinding.getRoot());
 
 
- variableBinding.button.setOnClickListener(new View.OnClickListener(){
+
+        //on click listener for button
+        variableBinding.button.setOnClickListener(new View.OnClickListener(){
         @Override
         public void onClick(View v){
             //tv.setText("You Clicked Button");
             //et.setText("You Clicked Button");
             //b.setText("You Clicked Button");
             viewModel.editTextContents = "You Clicked Button!";
+
+            variableBinding.editText.setText(viewModel.editTextContents);
+            variableBinding.TextviewHelloWorld.setText(viewModel.editTextContents);
+            variableBinding.button.setText(viewModel.editTextContents);
+            variableBinding.mySwitch.setChecked(true);
+            variableBinding.myRadio.setChecked(true);
+            variableBinding.myCheckBox.setChecked(true);
+
             //variableBinding.button.setText("You Clicked Button");
             //variableBinding.editText.setText("You Clicked Button");
             //variableBinding.TextviewHelloWorld.setText("You Clicked Button");
