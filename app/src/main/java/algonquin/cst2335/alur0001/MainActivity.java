cd26fadd36a8d202fe4b1565908474fc3cc4395e
Padma2code <padmavalli.aluri@gmail.com>
2023-01-28 19:54:10 -0500

    adjusted edittext and click button code

--- app/src/main/java/algonquin/cst2335/alur0001/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/alur0001/MainActivity.java
@@ -3,6 +3,15 @@ package algonquin.cst2335.alur0001;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/*Room T318, THursday 10am (Tomorrow)
+*/
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.button);
+        TextView tv =findViewById(R.id.TextviewHelloWorld);
+        EditText et = findViewById(R.id.editText);
+
     }
+
 }
