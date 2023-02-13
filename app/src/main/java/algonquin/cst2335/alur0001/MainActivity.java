b3bb2b0c7310a165b64114d32c4233e9b62e2990
Padma2code <padmavalli.aluri@gmail.com>
2023-02-12 20:52:48 -0500

    Finished task2 of lab 4

--- app/src/main/java/algonquin/cst2335/alur0001/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/alur0001/MainActivity.java
@@ -2,7 +2,10 @@ package algonquin.cst2335.alur0001;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
@@ -12,29 +15,51 @@ import android.widget.Switch;
 public class MainActivity extends AppCompatActivity {
 
     ImageView imgView;
+    private static String tag = "MainActivity";
+    private Object msg;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.w(tag, "In - On Resume - Loading Widgets");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w(tag, "In - On Pause - Loading Widgets");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w(tag, "In - On Destroy - Loading Widgets");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w(tag, "In - On Stop - Loading Widgets");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w(tag, "In - On Start - Loading Widgets");
+    }
+
     Switch sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        imgView = findViewById(R.id.flagView);
-        sw = findViewById(R.id.spin_switch);
-
-        sw.setOnCheckedChangeListener( (btn, isChecked) -> {
-            if (isChecked)
-            {
-                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-                rotate.setDuration(5000);
-                rotate.setRepeatCount(Animation.INFINITE);
-                rotate.setInterpolator(new LinearInterpolator());
-
-                imgView.startAnimation(rotate);
-            }
-            else {
-                imgView.clearAnimation();
-            }
+        View loginButton = null;
+        loginButton.setOnClickListener(clk -> {
+            Intent nextPage = new Intent( MainActivity.this, SecondActivity.class);
+            startActivity(nextPage);
         });
+
     }
 }
---
+++ app/src/main/java/algonquin/cst2335/alur0001/SecondActivity.java
@@ -0,0 +1,16 @@
+package algonquin.cst2335.alur0001;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+    }
+}
